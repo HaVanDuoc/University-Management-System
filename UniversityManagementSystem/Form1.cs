9b93b48661f96504a157708e76a0d971b72e2cee
Hà Văn Được <havanduoc.dev@gmail.com>
2022-07-11 15:53:41 +0700

    Add project files.

---
+++ UniversityManagementSystem/Form1.cs
@@ -0,0 +1,15 @@
+namespace UniversityManagementSystem
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
